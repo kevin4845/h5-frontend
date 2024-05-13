f6443075b306f39298c829f1f46bc50be93b96d9
NicoJBMortensen <n.bruunmortensen@gmail.com>
2024-05-13 14:32:48 +0200

    tilføjet en nav bar og rettet registerings siden så css mathcer

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("login").onclick = function(){
+    location.href = "/login/login.html";
+}
+document.getElementById("register").onclick = function(){
+    location.href = "/Registering/registering.html";
+}
