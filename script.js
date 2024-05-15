abbc8c7cfc8eb02dfe5b92fc39b8491dcbe7921b
NicoJBMortensen <n.bruunmortensen@gmail.com>
2024-05-15 09:13:39 +0200

    quick fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("login").onclick = function(){
-    location.href = "/login/login.html";
+    location.href = "login.html";
 }
 document.getElementById("register").onclick = function(){
-    location.href = "/Registering/registering.html";
+    location.href = "registering.html";
 }
